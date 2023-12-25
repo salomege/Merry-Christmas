a161802754ce18400201e67ae83264d14bdf6af8
salomege <salomegelenidze@gmail.com>
2023-12-25 02:09:43 -0800

    Initial Commit

---
+++ Merry Christmas/Merry_ChristmasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Merry_ChristmasApp.swift
+//  Merry Christmas
+//
+//  Created by salome on 25.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Merry_ChristmasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
